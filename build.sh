7bce0151747c3a6bf42f40121dcc515bc0485a99
Joyce <gathiithijoyce74@gmail.com>
2025-08-05 18:53:09 +0300

    aplly migrations,deleted some files

--- build.sh
+++ build.sh
@@ -5,8 +5,14 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
-python manage.py collectstatic --no-input
+# Make migrations
+python manage.py makemigrations
 
 # Apply database migrations
 python manage.py migrate
+
+# Create superuser if not exists
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell
+
+# Collect static files
+python manage.py collectstatic --no-input
